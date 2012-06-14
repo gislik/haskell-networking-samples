7b13d86dfa4c4341c37bd318dbeeb34da17a126a
Gísli Kristjánsson <gislik@hamstur.is>
2012-06-14 04:02:56 +0000

    Working prototype

--- IrcServer.hs
+++ IrcServer.hs
@@ -2,17 +2,18 @@ module Main where
 
 import System.Environment (getArgs)
 import System.IO (hClose, hPutStrLn, hGetLine, hSetBuffering, BufferMode(..))
-import System.IO.Error (catchIOError)
+import System.IO.Error (catchIOError, mkIOError, eofErrorType, ioError)
 import Control.Exception (finally)
 import Control.Concurrent (forkIO, killThread)
 import Control.Concurrent.Chan (Chan, newChan, writeChan, readChan, dupChan)
 import Network
 import Data.String (words, unwords)
 import Data.Maybe (Maybe (..))
+import Text.Printf (printf)
 
-data Message = Message {
-		channel :: Chan Message,
-		message :: String
+data ServerMessage = ServerMessage {
+		messageChannel :: ClientChannel,
+		messageCommand :: IrcCommand
 	}
 
 type Username 		= String
@@ -24,108 +25,154 @@ type RealName 		= String
 type QuitMessage 	= String
 type ChannelName 	= String
 type Receiver 		= String
+type MessageString	= String
+type ErrorString 	= String
 
 -- TODO: Ping Quit
-data IRCCommand = Pass Password
-				| Nick Nickname
+data IrcCommand = Nick Nickname
 				| User Username Hostname Servername RealName
 				| Ison Username
-				| Quit QuitMessage
-				| Join [ChannelName] [Password]
-				| Part [ChannelName]
-				| Privmsg [Receiver] String
-				| Notice Nickname String
-				| Unknown String
+				| Userhost Username
+				| Quit 
+				-- | Join [ChannelName] [Password]
+				-- | Part [ChannelName]
+				| Privmsg [Receiver] MessageString
+				-- | Notice Nickname String
+				| Unknown ErrorString
 
-instance Show IRCCommand where
-	show (Nick nick) = "NICK " ++ nick
-	show (User nick host server real) = "USER " ++ nick ++ " " ++ host ++ " " ++ server ++ " " ++ real
-	show (Ison user) = "ISON " ++ user
-	show (Privmsg [r] m) = "PRIVMSG " ++ r ++ " " ++ m
-	show (Unknown cmd) = "ERROR :Unknown command '" ++ cmd ++ "'"
-	show _ = "ERROR :server error"
-
-data IRCMessage = IRCMessage {
-		from :: Maybe String,
-		command :: IRCCommand
+data IrcMessage = IrcMessage {
+		ircMessageFrom :: Maybe String,
+		ircMessageCommand :: IrcCommand
 	}
 
-instance Show IRCMessage where
-	show (IRCMessage (Just from) command) = (':':from) ++ show command
-	show (IRCMessage Nothing command) = show command
+data IrcReply = RplWelcome 
+			  | RplUserhost
+			  | RplIson 
 
+instance Enum IrcReply where
+	fromEnum RplWelcome = 1
+	fromEnum RplUserhost = 302
+	fromEnum RplIson = 303
+
+	toEnum 1 = RplWelcome
+	toEnum 302 = RplUserhost
+	toEnum 303 = RplIson
+
+data IrcReplyMessage = IrcReplyMessage {
+		ircReplyMessageReply :: IrcReply,
+		ircReplyMessageNick :: Nickname,
+		ircReplyMessageString :: MessageString
+	}
+
+data IrcOldReply = IrcOldReply {
+		ircReplyNumber :: Int,
+		ircReplyMessage :: String
+	}
+
+
+type ClientChannel = Chan ClientMessage
+data Client = Client Nickname ClientChannel
+
+data ClientMessage = ClientCommand IrcCommand 
+				   | ClientReply IrcReplyMessage 
+				   | ClientPrivmsg Nickname Nickname MessageString
+				   | ClientError ErrorString
+				   | ClientQuit 
+		
+serverName = ":localhost"
 
 main = withSocketsDo $ do  -- only needed for win32 to initialize the networking subsystem
-	broadcastChannel <- newChan
 	serverChannel <- newChan
 	args <- getArgs
 	let port = read (head args) 
 	putStrLn "IrcServer starting..."
 	socket <- listenOn $ PortNumber (fromIntegral port)
-	acceptLoop socket serverChannel broadcastChannel `finally` sClose socket
+	forkIO $ serverLoop serverChannel [] 
+	acceptLoop socket serverChannel `finally` sClose socket
 
-acceptLoop socket serverChannel channel  = do
+acceptLoop socket serverChannel = do
 	(handle, _, _) <- accept socket
 	hSetBuffering handle LineBuffering
-	broadcastChannel <- dupChan channel
 	clientChannel <- newChan
 	clientId <- forkIO $ clientWriteLoop handle clientChannel
-	broadcastId <- forkIO $ clientBroadcastLoop handle broadcastChannel
 	putStrLn $ "Client " ++ show handle ++ " connected so I spawned " 
-				++ show clientId ++ " to handle writes and "
-				++ show broadcastId ++ " to handle broadcasts"
+				++ show clientId ++ " to handle writes"
 	forkIO $ clientReadLoop handle clientChannel serverChannel 
-				`catchIOError` errorHandler handle clientId broadcastId
+				`catchIOError` errorHandler handle clientId
 				`finally` hClose handle
-	forkIO $ serverLoop serverChannel broadcastChannel
-	acceptLoop socket serverChannel channel
+	acceptLoop socket serverChannel
 
-	where errorHandler h ct bt e = do 
+	where errorHandler h ct e = do 
 		putStrLn $ "Client " ++ show h ++ " disconnected so I am killing " ++ show ct
-						++ " and " ++ show bt 
 		killThread ct
-		killThread bt
 
 clientReadLoop handle clientChannel serverChannel = do
 	message <- hGetLine handle
 	putStrLn $ "Client " ++ show handle ++ " said: " ++ message
-	writeChan serverChannel (Message clientChannel message)
-	clientReadLoop handle clientChannel serverChannel 
-
+	let ircMessage = parseIrcMessage (words message)
+	case (ircMessageCommand ircMessage) of 
+		Quit 			-> do
+							writeChan serverChannel (ServerMessage clientChannel Quit)
+							ioError $ mkIOError eofErrorType "eof" Nothing Nothing
+		cmd 			-> do
+							writeChan serverChannel (ServerMessage clientChannel cmd)
+							clientReadLoop handle clientChannel serverChannel
+	
 clientWriteLoop handle clientChannel = do
-	Message channel message <- readChan clientChannel
-	hPutStrLn handle message
-	clientWriteLoop handle clientChannel
+	clientMessage <- readChan clientChannel
+	case clientMessage of 
+		ClientCommand (Nick nick) -> clientWriteLoop handle clientChannel
+		ClientReply (IrcReplyMessage ircReply nick reply) -> do 
+			hPutStrLn handle $ serverName ++ " " ++ (printf "%03d" (fromEnum ircReply)) ++ " " ++ nick ++ " " ++  reply
+			clientWriteLoop handle clientChannel
+		ClientPrivmsg from to msg -> do
+			hPutStrLn handle $ (':':from) ++ " " ++ "PRIVMSG " ++ to ++ " " ++ msg
+		ClientError err -> do
+			hPutStrLn handle $ "ERROR " ++ err
 
-clientBroadcastLoop handle broadcastChannel = do
-	Message channel message <- readChan broadcastChannel
-	hPutStrLn handle message
-	clientBroadcastLoop handle broadcastChannel
+serverLoop serverChannel clients = do
+	putStrLn $ "Server holds " ++ show (length clients)
+	ServerMessage clientChannel ircCommand <- readChan serverChannel
+	case ircCommand of 
+		Nick nick 	 		-> do 
+				putStrLn $ "Server is adding " ++ nick
+				let client = Client nick clientChannel
+				respond [client] ircCommand
+				serverLoop serverChannel $ client:clients
+		Quit   		 		-> serverLoop serverChannel $ filterNotChannel clientChannel
+		Privmsg (nick:_) m 	-> do
+				let (Client fromNick _) = head (filterChannel clientChannel)
+				respond (filterNick nick fromNick) ircCommand
+				serverLoop serverChannel clients
+		_      		 		-> do
+				respond (filterChannel clientChannel) ircCommand
+				serverLoop serverChannel clients 
 
-serverLoop serverChannel broadcastChannel = do
-	Message channel message' <- readChan serverChannel
-	let ircMessage = parseIRCMessage (words message')
-	-- respond serverChannel broadcastChannel channel (command message'') 
-	respond' channel ircMessage
-	serverLoop serverChannel broadcastChannel
-
-	where respond' channel imsg = respond serverChannel broadcastChannel channel (command imsg)
-
-respond :: Chan Message -> Chan Message -> Chan Message -> IRCCommand -> IO ()
-respond s b c (Nick _) = return ()
-respond s b c (User _ _ _ _) = writeChan b (Message s "001 gislik :Welcome")
-respond s b c cmd@(Privmsg _ _) = writeChan b (Message s (show cmd))
-respond s b c (Ison nick) = writeChan c (Message s ("303 unused :" ++ nick))
-respond s b c cmd@(Unknown _) = writeChan s (Message s (show cmd))
-respond _ _ _ _ = fail "This case is not handled"
-
-parseIRCMessage (s:ss) | head s == ':' = IRCMessage (Just (tail s)) (parse ss)
-parseIRCMessage ss = IRCMessage Nothing (parse ss)
+	where 
+		filterChannel c = filter (\(Client _ channel) -> channel == c) clients
+		filterNotChannel c = filter (\(Client _ channel) -> channel /= c) clients		
+		filterNick n fn = map (\(Client _ c) -> Client fn c) $ filter (\(Client nick _) -> nick == n) clients
 
 
 parse ("NICK":nick:_) = Nick nick
 parse ("USER":nick:host:server:real:_) = User nick host server real
-parse ("PASS":password:ss) = Pass password
-parse ("PRIVMSG":r:m:_) = Privmsg [r] m
+parse ("PRIVMSG":r:m) = Privmsg [r] (unwords m)
 parse ("ISON":nick:_) = Ison nick
+parse ("USERHOST":nick:_) = Userhost nick
+parse ("QUIT":_) = Quit 
 parse ss = Unknown (unwords ss)
+
+respond _ (User _ _ _ _) = return ()
+respond ((Client fromNick c):_) (Nick _) = writeChan c $ ClientReply (IrcReplyMessage  RplWelcome fromNick ":Welcome")
+respond ((Client fromNick c):[]) (Privmsg [toNick] msg) = writeChan c $ ClientPrivmsg fromNick toNick msg
+respond ((Client fromNick c):cs) cmd@(Privmsg [toNick] msg) = do
+									writeChan c $ ClientPrivmsg fromNick toNick msg
+									respond cs cmd
+respond ((Client fromNick c):_) (Ison nick) = writeChan c $ ClientReply (IrcReplyMessage RplIson fromNick nick)
+respond ((Client fromNick c):_) (Userhost nick) = writeChan c $ ClientReply (IrcReplyMessage RplUserhost fromNick (printf ":%s=-~%s@%s" nick nick "127.0.0.1"))
+respond ((Client _ c):_) (Unknown err) = writeChan c $ ClientError err
+respond [] _  = fail "No matching clients"
+respond _ _ = fail "This case is not handled"
+
+parseIrcMessage (s:ss) | head s == ':' = IrcMessage (Just (tail s)) (parse ss)
+parseIrcMessage ss = IrcMessage Nothing (parse ss)
